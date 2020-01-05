9b0bd3d53a9dfa99197f775739c1ecbc25591729
Cherissa Woudenberg <snerzel@comcast.net>
2020-01-05 20:09:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,9 +32,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-"totally"
+"arrays!"
 end
 
 def update_element_from_index(array, index_number, element)
-
+"totally"
 end
